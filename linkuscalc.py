7a79fae12bd9a7b01cec4085df9dcd2bba53c930
Linkus000 <linkus@openmailbox.org>
2018-08-13 13:01:19 +0200

    remove the sqrt functionality, and fix a bunch of bad code from those 3 years

--- linkuscalc.py
+++ linkuscalc.py
@@ -1,11 +1,10 @@
 import sys
-from mathfunctions import toBinary, toTrinary
+from mathfunctions import toBinary, toTernary
 from PyQt4 import QtGui
 from math import sqrt
 
 class Example(QtGui.QWidget):
     def __init__(self):
-
         super(Example, self).__init__()
         self.initUI()
         self.wie=4
@@ -19,33 +18,29 @@ class Example(QtGui.QWidget):
         combo = QtGui.QComboBox(self)
         combo.move(10, 80)
         combo.addItem("toBinary")
-        combo.addItem("toTrinary")
+        combo.addItem("toTernary")
         combo.addItem("fromBinary")
-        combo.addItem("fromTrinary")
-        combo.addItem("Square Root")
+        combo.addItem("fromTernary")
         combo.activated[str].connect(self.onTypeSwitch)
-        self.setWindowTitle("LinkusCalculator")
+        self.setWindowTitle("Very simple GUI 2,3 <--> 10 integer base converter")
         self.resize(400, 230)
         self.center()
         self.show()
     def onTypeSwitch(self, item):
-        if item=='Square Root': self.wie=1
-        elif item=='fromBinary': self.wie=2
-        elif item=='fromTrinary': self.wie=3
+        if item=='fromBinary': self.wie=2
+        elif item=='fromTernary': self.wie=3
         elif item=='toBinary': self.wie=4
         else: self.wie=5
     def onInput(self, text):
         try:
             if text=='': self.lbl.setText('')
             elif self.wie==4: self.lbl.setText(toBinary(text))
-            elif self.wie==5: self.lbl.setText(toTrinary(text))
-            elif self.wie==1: self.lbl.setText(str(sqrt(float(text))))
+            elif self.wie==5: self.lbl.setText(toTernary(text))
             else: self.lbl.setText(str(int(text, self.wie)))
         except ValueError:
             self.lbl.setText("Value Error!")
         self.lbl.adjustSize()
     def center(self):
-
         qr=self.frameGeometry()
         cp = QtGui.QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
